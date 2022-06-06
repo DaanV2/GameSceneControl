5dd9f927dea7d1c6728f4c2683639340b8fc68d7
Daan Verstraten <daan@q42.nl>
2022-06-06 14:55:44 +0200

    Adding new page route

--- GameSceneControl/AppShell.xaml.cs
+++ GameSceneControl/AppShell.xaml.cs
@@ -3,13 +3,13 @@ using GameSceneControl.Pages.Rooms;
 
 namespace GameSceneControl;
 
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
+public partial class AppShell : Shell {
+    public AppShell() {
+        this.InitializeComponent();
 
+        //Routes
         Routing.RegisterRoute(nameof(RoomsPage), typeof(RoomsPage));
         Routing.RegisterRoute(nameof(RoomPage), typeof(RoomPage));
+        Routing.RegisterRoute(nameof(NewRoomPage), typeof(NewRoomPage));
     }
 }
