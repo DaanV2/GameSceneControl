0f49aa87045ec42299e6d24d712c8f8aa1e28f6f
Daan Verstraten <daan@q42.nl>
2022-06-06 14:56:43 +0200

    Adding navigation handler for debugging

---
+++ GameSceneControl/Static Classes/Navigation/Navigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace GameSceneControl {
+    ///DOLATER <summary>add description for class: Navigation</summary>
+    public static partial class Navigation {
+        public static void GotoPage(String route) {
+            Task request = Shell.Current.GoToAsync(route);
+            HandleRequest(request);
+        }
+
+        public static void GotoPage(String route, IDictionary<String, Object> parameters) {
+            Task request = Shell.Current.GoToAsync(route, parameters);
+            HandleRequest(request);
+        }
+
+        [Conditional("DEBUG")]
+        private static void HandleRequest(Task request) {
+            _ = request.ContinueWith((t) => {
+                if (t.IsFaulted) {
+                    AggregateException ex = t.Exception;
+                    _ = Shell.Current.DisplayAlert("Error", ex.Message + "\n" + ex.StackTrace, "ok");
+                }
+            });
+        }
+    }
+}
