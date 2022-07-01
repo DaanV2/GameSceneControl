60a1c4f7d4c3cf5350f9dd1f87b9ff5163a8bd12
Daan Verstraten <daan@q42.nl>
2022-07-01 11:57:55 +0200

    Start on local controller

---
+++ Shared/Rooms/Controllers/Classes/LocalController/LocalController - Initialize.cs
@@ -0,0 +1,15 @@
+﻿using GameSceneControl.Services;
+using System.Collections.ObjectModel;
+
+namespace GameSceneControl.Rooms.Controllers {
+    ///DOLATER <summary>add description for class: LocalController</summary>
+    public partial class LocalController {
+        /// <summary>Creates a new instance of <see cref="LocalController"/></summary>
+        /// <param name="Info"></param>
+        public LocalController(RoomInfo Info) {
+            this.RoomInfo = Info;
+            this.State = ControllerState.Stopped;
+            this.Services = new ObservableCollection<ActiveService>();
+        }
+    }
+}
