188895feb5dd8dab4595ecd06f90461adbbe8d41
Daan Verstraten <daan@q42.nl>
2022-07-01 11:57:20 +0200

    Added error handling class

---
+++ Shared/Static Classes/ErrorHandling/ErrorHandling.cs
@@ -0,0 +1,35 @@
+﻿namespace GameSceneControl {
+    ///DOLATER <summary>add description for class: ErrorHandling</summary>
+    public static partial class ErrorHandling {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="Title"></param>
+        /// <param name="Message"></param>
+        public static void HandleError(Exception ex, String Title, String Message) {
+            //If we cant display it, then we need to just keep throwing it
+            if (Shell.Current is null) throw ex;
+
+#if DEBUG
+            Shell.Current.DisplayAlert($"Error: {Message}", $"{Message}\n{ex.Message}\n{ex.StackTrace}", "ok");
+
+            if (ex.InnerException is not null) HandleError(ex.InnerException, "Inner Exception", "This is an inner exception");
+#else
+            Shell.Current.DisplayAlert(Title, $"{Message}\n{ex.Message}", "ok");
+#endif
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="exceptions"></param>
+        /// <param name="Title"></param>
+        /// <param name="Message"></param>
+        public static void HandleError(AggregateException exceptions, String Title, String Message) {
+            foreach (Exception ex in exceptions.InnerExceptions) {
+                HandleError(ex, Title, Message);
+            }
+        }
+    }
+}
