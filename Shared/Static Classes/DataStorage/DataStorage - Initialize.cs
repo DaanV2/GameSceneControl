996144c73c88da7777017cf322bd258e1083fc38
Daan Verstraten <daan@q42.nl>
2022-07-01 11:57:34 +0200

    Updated data storage for windows

--- Shared/Static Classes/DataStorage/DataStorage - Initialize.cs
+++ Shared/Static Classes/DataStorage/DataStorage - Initialize.cs
@@ -1,9 +1,20 @@
-﻿namespace GameSceneControl {
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace GameSceneControl {
     ///DOLATER <summary>add description for class: DataStorage</summary>
     public static partial class DataStorage {
         public static void Initialize() {
+#if WINDOWS
+            String folder = Path.Join(
+                Environment.GetFolderPath(
+                    Environment.SpecialFolder.MyDocuments), 
+                    "GameSceneControl");
+            DataStorage.App = new DataFolder(folder);
+            DataStorage.Cache = new DataFolder(FileSystem.CacheDirectory);
+#else
             DataStorage.App = new DataFolder(FileSystem.AppDataDirectory);
             DataStorage.Cache = new DataFolder(FileSystem.CacheDirectory);
+#endif
         }
     }
 }
